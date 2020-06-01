6f324dc9e00fdd5d2dbca90d790f9ac64ca42031
Vishnu_Praveen <vishnupraveenblr@gmail.com>
2020-06-01 20:20:49 +0530

    Dynamic Window Slide

--- src/dynamicvariant/SmallestSumGreaterThanK.java
+++ src/dynamicvariant/SmallestSumGreaterThanK.java
@@ -1,4 +1,30 @@
 package dynamicvariant;
 
 public class SmallestSumGreaterThanK {
+
+    static int minSize(int[] arr, int k)
+    {
+        int minSize=Integer.MAX_VALUE;
+        int windowStart=0;
+
+        int currentWindowSum=0;
+
+        for (int windowEnd = 0; windowEnd <arr.length ; windowEnd++) {
+
+            currentWindowSum+=arr[windowEnd];
+
+            while(currentWindowSum>=k)
+            {
+                minSize=Math.min(minSize, windowEnd-windowStart+1);
+                currentWindowSum-=arr[windowStart];
+                windowStart++;
+            }
+        }
+        return minSize;
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println(minSize(new int[] {4,2,2,7,8,1,2,8,10},8));
+    }
 }
